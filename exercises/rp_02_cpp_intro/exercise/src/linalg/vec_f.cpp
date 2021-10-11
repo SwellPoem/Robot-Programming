d647ca8621d7fd6f7b35c4ee3a6d5b855fb95157
SwellPoem <val.piccione@stud.uniroma3.it>
2021-10-11 11:13:26 +0200

    ex changes

--- exercises/rp_02_cpp_intro/exercise/src/linalg/vec_f.cpp
+++ exercises/rp_02_cpp_intro/exercise/src/linalg/vec_f.cpp
@@ -73,6 +73,9 @@ VecF VecF::operator - (const VecF& other) const {
   assert(other.dim==dim && "dim mismatch");
   VecF returned(*this);
   // TODO: fillme
+  for(int i=0; i<dim; i++) {
+    returned.v[i]-=other.v[i];
+  }
   return returned;
 }
 
@@ -81,6 +84,9 @@ VecF VecF::operator - (const VecF& other) const {
 VecF  VecF::operator* (float f) const {
   VecF returned(*this);
   // TODO: fillme
+  for(int i=0; i<dim; i++) {
+    returned.v[i]*=f;
+  }
   return returned;
 }
 
