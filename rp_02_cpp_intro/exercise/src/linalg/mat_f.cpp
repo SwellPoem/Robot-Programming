6319929d07827e39f76f13e82e1b116c196fc307
Valentina Piccione <81562115+SwellPoem@users.noreply.github.com>
2023-10-10 09:31:05 +0000

    matrix_f updated, still miss matrix multiplication

--- rp_02_cpp_intro/exercise/src/linalg/mat_f.cpp
+++ rp_02_cpp_intro/exercise/src/linalg/mat_f.cpp
@@ -66,7 +66,15 @@ const float& MatF::at(int r, int c) const {
 //op =, deep copy
 MatF& MatF::operator =(const MatF& other) {
   // TODO: fillme
-
+  //DONE
+  rows = other.rows;
+  cols = other.cols;
+  dimension = other.dimension;
+  if(v!=nullptr)
+    free(v);
+  v = new float[dimension];
+  for (int i=0; i<dimension; ++i)
+    v[i]=other.at(i);
   return *this;
 }
 
@@ -74,38 +82,71 @@ MatF& MatF::operator =(const MatF& other) {
 MatF MatF::operator + (const MatF& other) const {
   assert (other.cols==cols && other.rows==rows && "dim mismatch");
   MatF returned (*this);
-
   // TODO: fillme
-
+  //DONE
+  for (int i=0; i<dimension; ++i)
+    returned.at(i)+=other.at(i);  //this could not be possible if i didn't have two implementations of the 'at' method
   return returned;
 }
 
 //returns the difference this - other
 MatF MatF::operator - (const MatF& other) const {
-
-    // TODO: fillme
-
+assert (other.cols==cols && other.rows==rows && "dim mismatch");
+  MatF returned (*this);
+  // TODO: fillme
+  //DONE
+  for (int i=0; i<dimension; ++i)
+    returned.at(i)-=other.at(i);  //same as for the '+' operator
+  return returned;
 }  
 
 
 // returns this*f
 MatF  MatF::operator* (float f) const {
+  MatF returned (*this);
   // TODO: fillme
+  //DONE
+  for (int i=0; i<dimension; ++i)
+    returned.at(i)*=f;
 }
 
-// returns this* other
+// returns this* other where the other is a vector
 VecF  MatF::operator* (const VecF& other) const {
   assert(other.dim==cols && "dim mismatch");
   VecF returned (rows);
   // TODO: fillme
+  //DONE
+  for (int r=0; r<rows; ++r) {
+    returned.at(r)=0;
+    for (int c=0; c<cols; ++c)
+      returned.at(r)+=at(r,c)*other.at(c);
+  }
+
+  //prof solution
+  // for (int i=0; i<rows; ++i) {
+  //   float acc = 0.0f;
+  //   for (int c=0; c<cols; ++c)
+  //     acc+=at(i,c)*other.at(c);
+  // returned.at(i)=acc;
+  //}
   return returned;
 }
 
-  // returns this* other
+  // returns this* other where the other is a matrix
 MatF  MatF::operator* (const MatF& other) const {
   assert(cols==other.rows && "dimension mismatch");
   MatF returned(rows, other.cols);
   //TODO: fillme
+  //DONE
+  for (int r=0; r<rows; ++r)
+    for (int c=0; c<other.cols; ++c) {
+      returned.at(r,c)=0;
+      for (int i=0; i<cols; ++i)
+        returned.at(r,c)+=at(r,i)*other.at(i,c);
+    }
+  
+  //prof solution
+ //see solutions 
   return returned;
 }
 
